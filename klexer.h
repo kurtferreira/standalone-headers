fe0670c5ad7d5fb7a9d4324eec228947afd3a93f
Kurt Ferreira <kurt@reload.co.za>
2025-05-27 15:45:12 +0200

    planning lexer

--- klexer.h
+++ klexer.h
@@ -1,5 +1,5 @@
 /*
- * This file is part of the standalone Lexer header file
+ * This file is part of the single-file header lexer library
  * 
  * Copyright (C) 2025 Kurt Ferreira
  *
@@ -17,4 +17,22 @@
  * along with this library. If not, see <https://www.gnu.org/licenses/>.
  */
 
- // @TODO
+/*
+Objectives:
+--------------------------------------------------------------------------------
+
+- we need to define error/stack tracing
+    Lexer_SetTracer(void (*cb)(const char *msg, const char *file, maxint_t line, maxint_t offset));
+- we need to identify file scopes
+    Lexer_DefineScript(const char *identifier);
+    Lexer_DefineScope(Lexer_Script *script, const char *scope);
+- we want to define some abritrary grammar that a script(s) should obey
+    Lexer_ExpectMatch(p_open_brace, p_close_brace, "Found open bracket with no closing bracket");
+- we want to define what crap is (comments) so ignore
+    Lexer_DefineIgnore(p_singleline_comment, P_Newline);
+    Lexer_DefineIgnore(p_multiline_comment_open, p_multiline_comment_close);
+- we need to define atomics
+    Lexer_DefineAtomic(?)
+- we need to define compile-time vs runtime stuff (@TODO)
+
+*/
